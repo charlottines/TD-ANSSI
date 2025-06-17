2c42aecce3e5038bfeeaf038dc132c64aafaa8a2
Anaëlle Pollart <128377927+anaelleplrt@users.noreply.github.com>
2025-06-17 12:29:46 +0200

    update alertes-mail

--- alertes-mail.py
+++ alertes-mail.py
@@ -1,11 +1,7 @@
 #étape 6 : génération d'alertes et d'email
 """Nous avons mis en place un système d’alerte pour identifier automatiquement les vulnérabilités critiques, en nous basant sur les critères suivants :
-
-    - Score CVSS ≥ 8 (Base_Severity = Critical)
-       OU
-    - EPSS ≥ 0.8
-
-Lorsqu’une vulnérabilité remplit ces conditions, un email d’alerte peut être généré pour informer rapidement l’équipe de sécurité."""
+Score CVSS ≥ 8 ou EPSS ≥ 0.8
+Lorsqu’une vulnérabilité remplit ces conditions, un email d’alerte est envoyé et un print dans la console est fait"""
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,8 +16,8 @@ df = pd.read_csv(csv_path, encoding="utf-8")
 
 #fonction qui permet d'envoyer l'email
 def send_email(to_email, subject, body):
-    from_email = "anaelle.pollart@gmail.com" #"votre_email@gmail.com"
-    password = "oahg ulrk oqsc tiyd" #"mot_de_passe_application"
+    from_email = "votre_email@gmail.com" 
+    password = "mot_de_passe_application"
 
     msg = MIMEText(body)
     msg['From'] = from_email
@@ -34,7 +30,7 @@ def send_email(to_email, subject, body):
     server.sendmail(from_email, to_email, msg.as_string())
     server.quit()
 
-# Préparation des alertes
+#préparation des alertes
 print("\n Simulation des alertes critiques par email")
 df["EPSS_score"] = pd.to_numeric(df["EPSS_score"], errors="coerce")
 df["CVSS_score"] = pd.to_numeric(df["CVSS_score"], errors="coerce")
@@ -47,15 +43,15 @@ for _, row in alertes.iterrows():
     lien = row["Lien_bulletin"]
     description = row["Description"]
     message = f"Une vulnérabilité critique a été détectetée sur {produit}.\nCVE : {cve}\nLien : {lien}\n\n{description}"
+
+    
     #affichage des alertes dans la console
     print(f"\n===== Alerte envoyée pour {cve} =====")
     print(message)
     print("============================================\n")
     
     #envoi alertes par email
-    send_email("anaelle.pollart@gmail.com", f"Alerte critique : {cve}", message)
-
-    #send_email("votre_email@gmail.com", f"Alerte critique : {cve}", message)
+    send_email("votre_email@gmail.com", f"Alerte critique : {cve}", message)
     
 
 
