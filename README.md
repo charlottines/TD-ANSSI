37e87960ea380539aacd1cf48cd1db89ce406ccc
Anaëlle Pollart <128377927+anaelleplrt@users.noreply.github.com>
2025-06-18 15:15:31 +0200

    update condition d'envoi des alertes et README

--- README.md
+++ README.md
@@ -19,7 +19,8 @@ Fichier qui permet la mise à jour du DataFrame.csv à partir des informations d
 
 ### 3️) alertes-mail.py
 
-Script de surveillance automatique des vulnérabilités critiques qui envoie une alerte (dans la console ou par mail) si le CVSS_score ≥ 8 ou EPSS_score ≥ 0.8
+Script de surveillance automatique des vulnérabilités critiques qui envoie une alerte (dans la console ou par mail) si le CVSS_score ≥ 8 ET EPSS_score ≥ 0.7
+Lorsqu’une vulnérabilité remplit ces deux conditions, un email d’alerte est envoyé et un print dans la console est fait. Cela permet de cibler uniquement les failles à la fois critiques et avec une forte probabilité d’attaque effective, donc vraiment prioritaires. 
 
 Par défaut, les alertes sont affichées dans la console. Pour activer l’envoi par email, décommente la ligne send_email(...) et configure ton adresse dans le script
 
@@ -38,7 +39,7 @@ S'assurer de bien avoir le fichier "Projet-Data-Update.py" dans le même répert
 Exécuter le fichier "Projet-Data.py" qui va récupérer les nouveaux bulletins sur le site de l'ANSSI et mettre à jour le DataFrame.csv en ajoutant ces nouveaux bulletins
 
 ### Etape 3 : 
-Exécuter le fichier "alertes-mail.py" : des alertes vont apparaître dans la console pour indiquer les vulnérabilités critiques. Il y a également la possibilité d'envoyer ces alertes par email en décommentant la ligne 55 (send_email(...)), en configurant l'adresse email et le mdp de l'envoyeur (lignes 20 et 21) et en indiquant l'adresse email du receveur lors de l'appel de la fonction send_email(...) ligne 55
+Exécuter le fichier "alertes-mail.py" : des alertes vont apparaître dans la console pour indiquer les vulnérabilités critiques. Il y a également la possibilité d'envoyer ces alertes par email en décommentant la ligne 66 (send_email(...)), en configurant l'adresse email et le mdp de l'envoyeur (lignes 22 et 23) et en indiquant l'adresse email du receveur lors de l'appel de la fonction send_email(...) ligne 66
 
 ### Etape 4 : 
 Regarder la page html qui correspond à notre fichier .ipynb (notre Notebook Jupyter) qui contient toute l’analyse des données de notre DataFrame.csv
--- alertes-mail.py
+++ alertes-mail.py
@@ -1,8 +1,10 @@
 
 # étape 6 : génération d'alertes et d'email
 
-# Nous avons mis en place un système d’alerte pour identifier automatiquement les vulnérabilités critiques, en nous basant sur les critères suivants : Score CVSS ≥ 8 ou EPSS ≥ 0.8
-# Lorsqu’une vulnérabilité remplit ces conditions, un email d’alerte est envoyé et un print dans la console est fait"""
+# Nous avons mis en place un système d’alerte pour identifier automatiquement les vulnérabilités critiques, en nous basant sur les critères suivants : 
+# Score CVSS ≥ 8 ET EPSS ≥ 0.7
+# Lorsqu’une vulnérabilité remplit ces deux conditions, un email d’alerte est envoyé et un print dans la console est fait
+# Cela permet de cibler uniquement les failles à la fois critiques et avec une forte probabilité d’attaque effective, donc vraiment prioritaires 
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,8 +19,8 @@ df = pd.read_csv(csv_path, encoding="utf-8")
 
 #fonction qui permet d'envoyer l'email
 def send_email(to_email, subject, body):
-    from_email = "votre_email@gmail.com" 
-    password = "mot_de_passe_application"
+    from_email = "votre_email@gmail.com"  #mettre l'email de l'envoyeur
+    password = "mot_de_passe_application" #mettre le mdp application de l'envoyeur
 
     msg = MIMEText(body)
     msg['From'] = from_email
@@ -36,25 +38,34 @@ print("\n Simulation des alertes critiques par email")
 df["EPSS_score"] = pd.to_numeric(df["EPSS_score"], errors="coerce")
 df["CVSS_score"] = pd.to_numeric(df["CVSS_score"], errors="coerce")
 
-alertes = df[(df["CVSS_score"] >= 8) | (df["EPSS_score"] >= 0.8)]
+#condition a respecter
+alertes = df[(df["CVSS_score"] >= 8) & (df["EPSS_score"] >= 0.7)]
 
-for _, row in alertes.iterrows():
+#affichage de l'alert dans la console
+for i, row in alertes.iterrows():
     produit = row["Produit"]
     cve = row["CVE_ID"]
     lien = row["Lien_bulletin"]
     description = row["Description"]
-    message = f"Une vulnérabilité critique a été détectetée sur {produit}.\nCVE : {cve}\nLien : {lien}\n\n{description}"
+    cvss = row["CVSS_score"]
+    epss = row["EPSS_score"]
 
+    print("\n" + "="*70)
+    print(f"ALERTE : Vulnérabilité critique détectée")
+    print("-" * 70)
+    print(f"Produit       : {produit}")
+    print(f"CVE ID        : {cve}")
+    print(f"Score CVSS    : {cvss}")
+    print(f"Score EPSS    : {epss}")
+    print(f"Lien Bulletin : {lien}")
+    print(f"Description   : {description.strip()[:300]}{'...' if len(description) > 300 else ''}")
+    print("="*70 + "\n")
 
-    #affichage des alertes dans la console
-    print(f"\n===== Alerte envoyée pour {cve} =====")
-    print(message)
-    print("============================================\n")
     
     #envoi alertes par email
     #send_email("votre_email@gmail.com", f"Alerte critique : {cve}", message)
 
-    #ligne 55 à décommenter si on veut envoyer par email (aussi mettre la bonne adresse email et mdp lignes 20 et 21)
+    #ligne 66 à décommenter si on veut envoyer par email (aussi mettre la bonne adresse email et mdp lignes 20 et 21)
     
 
 
