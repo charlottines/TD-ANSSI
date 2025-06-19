f2026c7bfb4506b65524344e2aa0b605c8c171d9
inesmeh <in.mehadhebi@gmail.com>
2025-06-19 12:30:00 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,2 +1,20 @@
+import os
+import subprocess
 import webbrowser
-webbrowser.open("http://127.0.0.1:8000")
+import time
+
+# Chemin absolu vers le dossier contenant manage.py
+project_dir = os.path.join(os.path.dirname(__file__), "projet_anssi")
+
+# Commande pour lancer le serveur Django
+def run_django():
+    subprocess.Popen(["python3", "manage.py", "runserver"], cwd=project_dir)
+
+# Lancer le serveur
+run_django()
+
+# Attendre que le serveur démarre
+time.sleep(2)
+
+# Ouvrir automatiquement dans le navigateur
+webbrowser.open("http://127.0.0.1:8000/")
